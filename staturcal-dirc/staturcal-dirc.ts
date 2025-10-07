50e6e5b1b28680f619f91ea381a841400f135644
ZishanZaman <shanibaloch2552@gmail.com>
2025-10-07 17:46:25 +0500

    ngIf or ngSwitch case

--- staturcal-dirc/staturcal-dirc.ts
+++ staturcal-dirc/staturcal-dirc.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 @Component({
-  selector: 'app-staturcal-dirc',
-  templateUrl: './staturcal-dirc.html',
-  styleUrl: './staturcal-dirc.css',
+  selector: "app-staturcal-dirc",
+  templateUrl: "./staturcal-dirc.html",
+  styleUrl: "./staturcal-dirc.css",
   imports: [CommonModule],
 })
 export class StaturcalDirc {
-  // public display = 1000 > 150;
-  public display = 'kumar';
-  public fruit = 'Mango';
+  public fruits = ["Banana", "Apple", "Mango", "Orange", "Grapes"];
+
+  public sports = [
+    {
+      name: "Cricket",
+      players: 11,
+      age: 5,
+    },
+    {
+      name: "Football",
+      players: 11,
+      age: 3,
+    },
+    {
+      name: "Hockey",
+      players: 11,
+      age: 4,
+    },
+  ];
+
+  // public Pizzaa = "Fajita";
+  // // public display = 1000 > 150;
+  // public display = 'kumar';
+  // public fruit = 'Mango';
 }
